9961628e09707a9ea0fd25cacea3bef3a46ceeed
Rees Amarteifio <reesamarteifio@gmail.com>
2024-11-30 13:24:29 -0600

    ok what we doin here

--- placeholders.js
+++ placeholders.js
@@ -1,16 +1,25 @@
 let message = "Type something skibidi";
 var acc = 0;
-var interval = 50;
-setInterval(()=> {
-    if(acc % message.length == 0){
-        acc = 0
-        interval = 400;
-        console.log("one done");
-    }
-    acc += 1
-    document.getElementById("uname").placeholder = message.slice((message.length - acc) % message.length);
-    if (interval != 50){
-        interval = 50
-    }
+var wait_duration = 10;
+var count = 0
+    setInterval(()=>{
+        if(acc % message.length == 0){
+            
+            acc = 0
+        }
+        if(count % message.length == 0) { 
+            wait = true;
+            count = 0
+        }
+        count += 1
+        if(!wait){
+            acc += 1
+            document.getElementById("uname").placeholder = message.slice((message.length - acc) % message.length);
+        }
+        else if(count == wait_duration)
+        {
+            wait = false;
+        }
+        
+    }, 50);
     //console.log(message.slice(-(message.length - acc) % message.length))
-}, interval);
